2f828e82ba04af9fe9527680a92169a573095ecf
ikmgputrawijaya <putrawijaya537@gmail.com>
2023-05-29 21:51:08 +0800

    First commit menghubungkan http server

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,46 @@
-package org.example;public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+package org.example;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        HttpServer httpServer = HttpServer.create(new InetSocketAddress("localhost", 8082), 0);
+
+        httpServer.createContext("/ecommerce", new EcommerceHandler());
+        httpServer.setExecutor(Executors.newSingleThreadExecutor());
+        httpServer.start();
+    }
+
+    public static class EcommerceHandler implements HttpHandler{
+
+        @Override
+        public void handle(HttpExchange exchange) throws IOException{
+            // POST -> create
+            // Get -> read
+            // PUT -> update
+            // DELETE -> delete
+            // CRUD
+
+            if("GET".equals(exchange.getRequestMethod())) {
+                // send something interesting to the user
+                OutputStream outputStream = exchange.getResponseBody();
+                String responseToBeSentBack = "Hello from the other side";
+                exchange.sendResponseHeaders(200, responseToBeSentBack.length());
+
+                outputStream.write(responseToBeSentBack.getBytes());
+                outputStream.flush();
+                outputStream.close();
+
+
+            }
+        }
     }
 }
