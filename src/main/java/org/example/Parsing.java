a07f8a4db1c2e9234fb49dc98930623d6b66e15f
ikmgputrawijaya <putrawijaya537@gmail.com>
2023-05-30 17:55:23 +0800

    menambahkan kelas Parsing.java

---
+++ src/main/java/org/example/Parsing.java
@@ -0,0 +1,35 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Parsing {
+    public static String[] splitPath(String path) {
+        return path.split("/");
+    }
+
+    public static boolean hasFilter(String string) {
+        return string.contains("?");
+    }
+
+    public static String[] splitFilter(String filter) {
+        return filter.split("&");
+    }
+
+    public static String[] splitFilterCondition(String condition) {
+        return condition.split("=");
+    }
+
+    public static List<String> extractFilterConditions(String filter) {
+        List<String> conditions = new ArrayList<>();
+        String[] filterParts = splitFilter(filter);
+
+        for (String part : filterParts) {
+            String[] condition = splitFilterCondition(part);
+            if (condition.length == 2) {
+                conditions.add(condition[0] + "=" + condition[1]);
+            }
+        }
+        return conditions;
+    }
+}
--- src/main/java/org/example/Struktur.java
+++ src/main/java/org/example/Struktur.java
@@ -52,4 +52,3 @@ public class Struktur {
         return result;
     }
 }
-
